5855de826ddc9cf788830804a3bf8574e8b333c9
Luis <luish000.lh@gmail.com>
2018-01-06 22:47:16 -0400

    cleaned the index.js file

--- index.js
+++ index.js
@@ -2,24 +2,4 @@ const FenwickTree = require('./fenwick-tree.js')
 
 const array = [[1, 2], [3, 4]]
 
-const tree = new FenwickTree([4, 4, 4])
-
-
-// const k = tree.alternative()
-
-// k[0][0][0] = 1
-
-// console.log(k)
-
-tree.update([1, 1, 1], 4)
-tree.update([3, 3, 3], 23)
-tree.update([4, 4, 4], 24)
-// tree.update([2, 1], 2)
-// tree.update([1, 2], 3)
-// tree.update([2, 2], 4)
-
-// [[], []]
-
-// tree[0][0][0] = 1
-
-console.log(tree.sum([4, 4, 4]))
+const tree = new FenwickTree([2, 2])
